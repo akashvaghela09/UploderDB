73d6a358e36b2cbc89c462037ad69a224008c63a
akashvaghela09 <akashvaghela09@gmail.com>
2021-05-06 02:45:49 +0530

    CORS header added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const app = express();
 const filesUploadController = require("./controller/filesUpload.controller")
 
 app.use(express.json())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    next();
+});
 
 app.use("/files", filesUploadController)
 
