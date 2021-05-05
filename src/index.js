b968e0833dd3945ddcce9021df32bb3000938ad9
akashvaghela09 <akashvaghela09@gmail.com>
2021-05-06 03:37:19 +0530

    CORS package added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 const filesUploadController = require("./controller/filesUpload.controller")
 
 app.use(express.json())
+app.use(cors());
+
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Credentials", "true");
-    res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-});
+  });
 
 app.use("/files", filesUploadController)
 
