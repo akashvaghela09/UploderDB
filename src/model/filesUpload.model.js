43c9ca000d0658d0b150e6d21b4301c4d69404b0
akashvaghela09 <akashvaghela09@gmail.com>
2021-06-02 09:47:24 +0530

    schema changed for file upload

--- src/model/filesUpload.model.js
+++ src/model/filesUpload.model.js
@@ -4,10 +4,8 @@ const filesUploadSchema = new mongoose.Schema(
     {
         email: {type:String, required: true},
         code: {type:String, required: true},
-        adminCode: {type:String, required: true},
         fileName: {type:String, required: true},
         fileSize: {type:Number, required: true},
-        md5: {type:String, required: true},
         directLink: {type:String, required: true},
         downloadPage: {type:String, required: true}
     }, 
